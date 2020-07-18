d887f1b51d37f96b65709d7879b3de3c3d1aff50
Shamsulhaq <bmshamsulhaq65@gmail.com.com>
2020-07-18 15:40:42 +0600

    Add DRF and Create API View

--- src/event/views.py
+++ src/event/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ListSerializer
+from .models import List
+
 
 # Create your views here.
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = List.objects.all().order_by('keyword')
+    serializer_class = ListSerializer
--- src/todo/settings.py
+++ src/todo/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Installed Apps 
     'event',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
